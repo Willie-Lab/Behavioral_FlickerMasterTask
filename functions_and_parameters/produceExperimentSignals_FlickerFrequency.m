001da865079dd1d85993b0f3c0c9eb85ec0efcd3
Lou Blanpain <lou.blanpain@emory.edu>
2021-07-13 13:29:04 -0400

    modified so have safety testing for flickerfrequencytask (of all frequencies we're going to run)

--- functions_and_parameters/produceExperimentSignals_FlickerFrequency.m
+++ functions_and_parameters/produceExperimentSignals_FlickerFrequency.m
@@ -45,7 +45,7 @@
 % * 1 field, 'sample_rate' indicating the sample rate of the signal, to be used on the NIDAQ.
 %
 
-function sourceSignal=produceExperimentSignals_FlickerFrequency(duty_cycle_LED,duty_cycle_sound,sound_frequency,type_of_burst,sample_rate,frequencies_tested,modalities_tested,repeat_limit,stimPeriod_duration,postStimPeriod,numberOfTrials_perCondition,nber_sessions,trial_random)
+function sourceSignal=produceExperimentSignals_FlickerFrequency(safetyTesting,duty_cycle_LED,duty_cycle_sound,sound_frequency,type_of_burst,sample_rate,frequencies_tested,modalities_tested,repeat_limit,stimPeriod_duration,postStimPeriod,numberOfTrials_perCondition,nber_sessions,trial_random)
     
     %% Set parameters of randomization
     randomFlicker_frequency=40; %picking average of 40Hz for random flicker
@@ -56,9 +56,14 @@ function sourceSignal=produceExperimentSignals_FlickerFrequency(duty_cycle_LED,d
     sourceSignal.sample_rate=sample_rate;
     for i=1:length(frequencies_tested)
         if frequencies_tested(i)==-1 %means random
-            for j=1:numberOfTrials_perCondition
-                sourceSignal.Rand{j}=produceSignals(stimPeriod_duration,randomFlicker_frequency,1,duty_cycle_LED,duty_cycle_sound,sound_frequency,type_of_burst,sourceSignal.sample_rate);
-                sourceSignal.Rand{j}=[sourceSignal.Rand{j};zeros(sourceSignal.sample_rate*postStimPeriod,2)];
+            if ~safetyTesting
+                for j=1:numberOfTrials_perCondition
+                    sourceSignal.Rand{j}=produceSignals(stimPeriod_duration,randomFlicker_frequency,1,duty_cycle_LED,duty_cycle_sound,sound_frequency,type_of_burst,sourceSignal.sample_rate);
+                    sourceSignal.Rand{j}=[sourceSignal.Rand{j};zeros(sourceSignal.sample_rate*postStimPeriod,2)];
+                end
+            elseif safetyTesting %only need to create 1 random frequency trial
+                sourceSignal.Rand{1}=produceSignals(stimPeriod_duration,randomFlicker_frequency,1,duty_cycle_LED,duty_cycle_sound,sound_frequency,type_of_burst,sourceSignal.sample_rate);
+                sourceSignal.Rand{1}=[sourceSignal.Rand{1};zeros(sourceSignal.sample_rate*postStimPeriod,2)];
             end
         elseif frequencies_tested(i)==0 %means baseline
             sourceSignal.Baseline=zeros(sourceSignal.sample_rate*(stimPeriod_duration+postStimPeriod),2);
@@ -67,63 +72,66 @@ function sourceSignal=produceExperimentSignals_FlickerFrequency(duty_cycle_LED,d
         end
     end
     
-    %% Set order of trials:
-    %set correct names of conditions:
-    temp_frequencies_tested=frequencies_tested;
-    temp_frequencies_tested=strtrim(string(num2str(temp_frequencies_tested')));
-    temp_frequencies_tested(~ismember(temp_frequencies_tested,'-1'))=strcat(temp_frequencies_tested(~ismember(temp_frequencies_tested,'-1')),'Hz');
-    temp_frequencies_tested(ismember(temp_frequencies_tested,'-1'))='R';
-    if any(ismember(temp_frequencies_tested,'0Hz'))
-        baseline_present=1;
-        temp_frequencies_tested(ismember(temp_frequencies_tested,'0Hz'))=[];
-    end
     
-    if strcmp(trial_random,'pseudorandom')
-        %create a random vector of trials for the experiment (with no more than x repetitions of the same modality or frequency):
-        temp=pseudorandomization({temp_frequencies_tested;modalities_tested},repeat_limit,numberOfTrials_perCondition);
-        
-        %add randomly interspersed baseline trials if needed:
-        if baseline_present
-            satisfied=0;
-            while ~satisfied
-                baseline_rand=sort(randsample(length(temp),numberOfTrials_perCondition)); %randomize indices where baseline trials will be inserted
-                if ~any(baseline_rand(end:-1:1)-[baseline_rand(end-1:-1:1);0]<round(length(temp)/numberOfTrials_perCondition/2)) %we want baseline trials to be separated by reasonable amount of trials
-                    satisfied=1;
+    %% Set order of trials (in case we are running experiment, i.e. not necessary for safetyTesting):
+    if ~safetyTesting
+        %set correct names of conditions:
+        temp_frequencies_tested=frequencies_tested;
+        temp_frequencies_tested=strtrim(string(num2str(temp_frequencies_tested')));
+        temp_frequencies_tested(~ismember(temp_frequencies_tested,'-1'))=strcat(temp_frequencies_tested(~ismember(temp_frequencies_tested,'-1')),'Hz');
+        temp_frequencies_tested(ismember(temp_frequencies_tested,'-1'))='R';
+        if any(ismember(temp_frequencies_tested,'0Hz'))
+            baseline_present=1;
+            temp_frequencies_tested(ismember(temp_frequencies_tested,'0Hz'))=[];
+        end
+
+        if strcmp(trial_random,'pseudorandom')
+            %create a random vector of trials for the experiment (with no more than x repetitions of the same modality or frequency):
+            temp=pseudorandomization({temp_frequencies_tested;modalities_tested},repeat_limit,numberOfTrials_perCondition);
+
+            %add randomly interspersed baseline trials if needed:
+            if baseline_present
+                satisfied=0;
+                while ~satisfied
+                    baseline_rand=sort(randsample(length(temp),numberOfTrials_perCondition)); %randomize indices where baseline trials will be inserted
+                    if ~any(baseline_rand(end:-1:1)-[baseline_rand(end-1:-1:1);0]<round(length(temp)/numberOfTrials_perCondition/2)) %we want baseline trials to be separated by reasonable amount of trials
+                        satisfied=1;
+                    end
+                end
+                for i=1:length(baseline_rand) %insert baseline trials
+                    temp=[temp(1:baseline_rand(i)-1) "Baseline" temp(baseline_rand(i):end)];
+                    baseline_rand=baseline_rand+1;
                 end
             end
-            for i=1:length(baseline_rand) %insert baseline trials
-                temp=[temp(1:baseline_rand(i)-1) "Baseline" temp(baseline_rand(i):end)];
-                baseline_rand=baseline_rand+1;
+
+        elseif strcmp(trial_random,'distributed_random')
+            nber_stimconditions=length(temp_frequencies_tested)*length(modalities_tested);
+            temp=strings(1,numberOfTrials_perCondition*nber_stimconditions);
+
+            for i=0:numberOfTrials_perCondition-1
+                temp(nber_stimconditions*i+1:nber_stimconditions*i+nber_stimconditions)=pseudorandomization({temp_frequencies_tested;modalities_tested},repeat_limit,1);
+            end
+
+            if baseline_present
+                baseline_rand=sort(datasample(1:nber_stimconditions,numberOfTrials_perCondition)); %randomly sample from 1 to number of stim conditions, with replacement, a numberOfTrials_perCondition times
+                for i=1:length(baseline_rand) %insert baseline trials
+                    temp=[temp(1:(i-1)*(nber_stimconditions+1)+baseline_rand(i)-1) "Baseline" temp((i-1)*(nber_stimconditions+1)+baseline_rand(i):end)];
+                end
             end
         end
-        
-    elseif strcmp(trial_random,'distributed_random')
-        nber_stimconditions=length(temp_frequencies_tested)*length(modalities_tested);
-        temp=strings(1,numberOfTrials_perCondition*nber_stimconditions);
-        
-        for i=0:numberOfTrials_perCondition-1
-            temp(nber_stimconditions*i+1:nber_stimconditions*i+nber_stimconditions)=pseudorandomization({temp_frequencies_tested;modalities_tested},repeat_limit,1);
-        end
-        
-        if baseline_present
-            baseline_rand=sort(datasample(1:nber_stimconditions,numberOfTrials_perCondition)); %randomly sample from 1 to number of stim conditions, with replacement, a numberOfTrials_perCondition times
-            for i=1:length(baseline_rand) %insert baseline trials
-                temp=[temp(1:(i-1)*(nber_stimconditions+1)+baseline_rand(i)-1) "Baseline" temp((i-1)*(nber_stimconditions+1)+baseline_rand(i):end)];
+
+        %divide trials up into x sessions:
+        trials_vector=strings(ceil(length(temp)/nber_sessions),nber_sessions); %initialize vector of trials: 6 columns, each corresponding to a session in this experiment (total of 6 sessions, ideally 10min each, per experiment)
+        for i=1:nber_sessions
+            if i~=nber_sessions
+                trials_vector(1:ceil(length(temp)/nber_sessions),i)=temp((i-1)*ceil(length(temp)/nber_sessions)+1:i*ceil(length(temp)/nber_sessions));
+            elseif i==nber_sessions
+                trials_vector(1:length(temp((i-1)*ceil(length(temp)/nber_sessions)+1:end)),i)=temp((i-1)*ceil(length(temp)/nber_sessions)+1:end);
             end
         end
+
+        sourceSignal.trials_vector=trials_vector; %add information about trials into the main structure
     end
-    
-    %divide trials up into x sessions:
-    trials_vector=strings(ceil(length(temp)/nber_sessions),nber_sessions); %initialize vector of trials: 6 columns, each corresponding to a session in this experiment (total of 6 sessions, ideally 10min each, per experiment)
-    for i=1:nber_sessions
-        if i~=nber_sessions
-            trials_vector(1:ceil(length(temp)/nber_sessions),i)=temp((i-1)*ceil(length(temp)/nber_sessions)+1:i*ceil(length(temp)/nber_sessions));
-        elseif i==nber_sessions
-            trials_vector(1:length(temp((i-1)*ceil(length(temp)/nber_sessions)+1:end)),i)=temp((i-1)*ceil(length(temp)/nber_sessions)+1:end);
-        end
-    end
-    
-    sourceSignal.trials_vector=trials_vector; %add information about trials into the main structure
 
 end
 
